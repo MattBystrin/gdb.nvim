a2fa6b5e81132e5b255bad1c67d2481456eb9969
Matthew Bystrin <bistrin.matvey@gmail.com>
2023-03-21 21:35:05 +0300

    Start develop of breakpoints module

--- lua/gdb/modules/bkpts/init.lua
+++ lua/gdb/modules/bkpts/init.lua
@@ -3,41 +3,39 @@ local M = require('gdb.modules.iface'):new()
 local log = require('gdb.log')
 
 M.name = "breakpoints"
+M.files = {}
 
 function M:on_attach()
-	self.files = {}
+	M.files = {}
 end
 
 function M:on_detach()
-	self.files = nil
+	M.files = nil
 end
 
-local get_bp_info
 local parse
 local parse_list
+local delete_handler
 
-parse = function(str)
-	log.trace('breakpoint event')
-	-- Can be created modified and deleted
-	if str:match('delete') then
-		local id = tonumber(str:match('id="([^"]+)'))
-		for _,v in ipairs(M.files) do
-			if v.bps[id] then
-				v.bps[id] = nil
-			end
-		end
-	elseif  str:match('modified') then
-	elseif  str:match('created') then
-	end
-	local file, line = get_bp_info(str)
-	return {file = file, line = line}
+-- '=breakpoint-created,bkpt={...}'
+--  '=breakpoint-modified,bkpt={...}'
+--  '=breakpoint-deleted,id=NUMBER'
+--
+
+function M.parsers()
+	return {
+		{ pattern = '^=breakpoint%-created', handler = parse },
+		{ pattern = '^=breakpoint%-modified', handler = parse },
+		{ pattern = '^=breakpoint%-deleted', handler = delete_handler },
+		{ pattern = '^%^done,BreakpointTable', handler = parse_list}
+	}
 end
 
-get_bp_info = function(str)
+local function get_bp_info(str)
 	local file = str:match('fullname="([^"]+)')
 	local line = tonumber(str:match('line="([^"]+)'))
 	local id = tonumber(str:match('number="([^"]+)'))
-	local times = tonumber(str:match('times="([^"]+)'))
+	local hits = tonumber(str:match('times="([^"]+)'))
 	local en = str:match('enabled="([^"]+)') == 'y'
 	local cond = str:match('cond="([^"]+)')
 	if not file or not id then return end
@@ -53,38 +51,48 @@ get_bp_info = function(str)
 	local bp = M.files[file].bps[id]
 	bp.line = line
 	bp.en = en
-	bp.times = times
+	bp.hits = hits
 	bp.cond = cond
 	log.debug('bps updated', M.files)
 	return file, line
 end
 
+parse = function(str)
+	log.trace('breakpoint event')
+	-- Can be created modified and deleted
+	local file, line = get_bp_info(str)
+	return {file = file, line = line}
+end
+
+delete_handler = function(str)
+	local id = tonumber(str:match('id="([^"]+)'))
+	if not id then return end
+
+	for _,v in ipairs(M.files) do
+		if v.bps[id] then
+			v.bps[id] = nil
+		end
+	end
+end
+
+
 parse_list = function(str)
 	for s in str:gmatch("bkpt=(%b{})") do
 		get_bp_info(s)
 	end
-	log.trace('bps table')
 end
 
-M.parsers = {
-	{
-		pattern = '=breakpoint',
-		pfunc = parse
-	},
-	{
-		pattern = '=breakpoint-table',
-		pfunc = parse_list
-	}
-}
 
-function M.bkpt()
+-- Add/delete breakpoint
+local function bkpt()
 	-- local line = unpack(vim.api.nvim_win_get_cursor(0))
 	-- local file = vim.api.nvim_buf_get_name(0) -- Current buf
 	-- vim.api.nvim_echo({ { 'toggle bp' .. file .. ':' .. line } }, false, {})
 	vim.api.nvim_echo({ { 'not implemented yet' } }, false, {})
 end
 
-function M.bkpt_en()
+-- Enable/diasble breakpoint
+local function bkpt_en()
 	-- local line = unpack(vim.api.nvim_win_get_cursor(0))
 	-- local file = vim.api.nvim_buf_get_name(0) -- Current buf
 	--vim.api.nvim_echo({ { 'bkpt_en in ' .. file .. ':' .. line } }, false, {})
---
+++ tests/bkpts_spec.lua
@@ -0,0 +1,62 @@
+local function parse_mock(str, mod)
+	for _,p in ipairs(mod:parsers()) do
+		if str:find(p.pattern) then
+			p.handler(str)
+			return
+		end
+	end
+end
+
+describe('breakpoints module ->', function()
+
+local mod = require'gdb.modules.bkpts'
+
+it('breakpoint created', function()
+	mod:on_attach()
+	local str = '=breakpoint-created,bkpt={number="2",type="breakpoint",disp="keep",enabled="y",addr="0x000000000000119a",func="some_func",file="tests/intgr/c/test.c",fullname="/home/ronin/Develop/gdb.nvim/tests/intgr/c/test.c",line="24",thread-groups=["i1"],times="0",original-location="some_func"}'
+	parse_mock(str, mod)
+	assert.same({
+		["/home/ronin/Develop/gdb.nvim/tests/intgr/c/test.c"] = {
+			bps = {
+				[2] = {
+					en = true,
+					line = 24,
+					hits = 0
+				}
+			}
+		}
+	}, mod.files)
+	mod:on_detach()
+end)
+
+it('breakpoint modified', function()
+	mod:on_attach()
+	local str = '=breakpoint-modified,bkpt={number="1",type="breakpoint",disp="keep",enabled="y",addr="0x00005555555551ca",func="main",file="tests/intgr/c/test.c",fullname="/home/ronin/Develop/gdb.nvim/tests/intgr/c/test.c",line="33",thread-groups=["i1"],times="1",original-location="main"}\r'
+	parse_mock(str, mod)
+	assert.same({
+		["/home/ronin/Develop/gdb.nvim/tests/intgr/c/test.c"] = {
+			bps = {
+				[1] = {
+					en = true,
+					line = 33,
+					hits = 1
+				}
+			}
+		}
+	}, mod.files)
+	mod:on_detach()
+end)
+
+-- it('breakpoint deleted', function()
+-- 	local str = '=breakpoint-deleted,id="1"\r'
+-- 	parse_mock(str, mod)
+-- 	assert.same({}, {})
+-- end)
+-- it('breakpoint list', function()
+-- 	local str = '^done,BreakpointTable={nr_rows="2",nr_cols="6",hdr=[{width="7",alignment="-1",col_name="number",colhdr="Num"},{width="14",alignment="-1",col_name="type",colhdr="Type"},{width="4",alignment="-1",col_name="disp",colhdr="Disp"},{width="3",alignment="-1",col_name="enabled",colhdr="Enb"},{width="18",alignment="-1",col_name="addr",colhdr="Address"},{width="40",alignment="2",col_name="what",colhdr="What"}],body=[bkpt={number="2",type="breakpoint",disp="keep",enabled="y",addr="0x000055555555519a",func="some_func",file="tests/intgr/c/test.c",fullname="/home/ronin/Develop/gdb.nvim/tests/intgr/c/test.c",line="24",thread-groups=["i1"],times="0",original-location="some_func"},bkpt={number="3",type="breakpoint",disp="keep",enabled="y",addr="0x00005555555551ca",func="main",file="tests/intgr/c/test.c",fullname="/home/ronin/Develop/gdb.nvim/tests/intgr/c/test.c",line="33",thread-groups=["i1"],times="0",original-location="main"}]}\r'
+-- 	parse_mock(str, mod)
+-- 	assert.same({}, {})
+-- end)
+mod:on_detach()
+
+end)
